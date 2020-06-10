9e72412bee9820be89ba8d4ab9a57df62fe9ce0e
Oksana Polishchuk <56505077+OksanaPolishchuk@users.noreply.github.com>
2020-06-10 21:05:15 +0300

    Implement translate method

---
+++ CarInfoTelegramBot/Bots/English/EnglishTelegramBotTest/Services/MessageProcessorTests.cs
@@ -0,0 +1,29 @@
+﻿using EnglishDbService;
+using EnglishTelegramBot.Services;
+using LoggerCommon;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace EnglishTelegramBotTest.Services
+{
+    [TestClass]
+    public class MessageProcessorTests
+    {
+        private MessageProcessor _target;
+
+        [TestInitialize]
+        public void Init()
+        {
+            var englishRepository = Substitute.For<IEnglishRepository>();
+            var logger = Substitute.For<ILogger>();
+            _target = new MessageProcessor(englishRepository, logger);
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            var actual = _target.Process("стул", 0).Result;
+            Assert.AreEqual("chair", actual);
+        }
+    }
+}
